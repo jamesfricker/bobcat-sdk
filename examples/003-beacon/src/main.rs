57e1644338e0e0e96774d3ee2de110becc4551fd
bayge <alex@fluidity.money>
2025-10-15 02:12:20 +0900

    Fix mistake in the beacon slicing

--- examples/003-beacon/src/main.rs
+++ examples/003-beacon/src/main.rs
@@ -17,7 +17,7 @@ pub unsafe extern "C" fn user_entrypoint(args_len: usize) -> usize {
     if args[..4] != SEL {
         return 1;
     }
-    let beacon = read_word_slices!(args, 1);
+    let beacon = read_word_slices!(&args[4..], 1);
     let r = create1_slice::<1024>(&make_beacon_proxy(beacon.into()), U::ZERO);
     write_result_exit_create!(r)
 }
