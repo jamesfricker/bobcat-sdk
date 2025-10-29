c9b2a5740ae6a619625da4af3781dce871757c05
bayge <alex@fluidity.money>
2025-10-29 16:27:31 +0900

    Have some discussion of the different math function performance implications

--- bobcat-maths/README.md
+++ bobcat-maths/README.md
@@ -4,11 +4,16 @@
 Anecdotally, the order of costs for the various muldiv functions look like this, when we
 used it in the 9lives repo:
 
-|            Name          |                          Description                          |  Codesize cost | Gas cost |
-|--------------------------|---------------------------------------------------------------|----------------|----------|
-| Widening then truncating | A normal muldiv operation similar to Uniswap's implementation | Cheapest       | Middling |
-| Widening like Uniswap    |
-| Ruint                    | Ruint-powered muldiv                                          | Most expensive | Best     |
+|            Name           |   Precision      |  Codesize cost   | Estimated 9lives mint gas cost |
+|---------------------------|------------------|------------------|--------------------------------|
+| Muldiv with Euclidian GCD | Poor             | Very cheap       | 338363                         |
+| Muldiv Uniswap            | Perfect          | Moderate         | 337454                         |
+| Widening then truncating  | Perfect          | Cheap            | 336378                         |
+| Ruint                     | Perfect          | Expensive        | 334110                         |
 
 So, you should make a decision based on the context of what you're working on, and do some
-experimentation yourself.
+experimentation yourself. Ruint works with 64 bit words, then branches to pick different
+algorithms. 9lives uses muldiv for fee taking operations, with numbers of either 1e6,
+1000, and 1e12. You need to enable ruint with `ruint-enabled`. It might be more
+appropriate to use ruint if you're already using U256 from alloy, given the amount of code
+reuse presumably.
--- bobcat-maths/src/lib.rs
+++ bobcat-maths/src/lib.rs
@@ -32,7 +32,7 @@ unsafe extern "C" {
 }
 
 #[cfg(feature = "ruint-enabled")]
-use alloy_primitives::{U256, ruint};
+use alloy_primitives::{ruint, U256};
 
 #[cfg(feature = "alloy-enabled")]
 mod alloy {
@@ -291,6 +291,37 @@ pub fn saturating_mul(x: &U, y: &U) -> U {
     checked_mul(x, y).unwrap_or(U::MAX)
 }
 
+fn gcd(mut x: U, mut y: U) -> U {
+    while y.is_some() {
+        (x, y) = (y, x % y);
+    }
+    x
+}
+
+pub fn gcd_mul_div(x: &U, y: &U, denom: U) -> Option<(U, bool)> {
+    if denom.is_zero() {
+        return None;
+    }
+    let g1 = gcd(*x, denom);
+    let x_reduced = x / &g1;
+    let denom_reduced = denom / g1;
+    let g2 = gcd(*y, denom_reduced);
+    let y_reduced = y / &g2;
+    let denom_final = denom_reduced / g2;
+    let product = x_reduced.checked_mul(&y_reduced)?;
+    let result = (x_reduced * y_reduced) / denom_final;
+    let remainder = product % denom_final;
+    Some((result, remainder.is_some()))
+}
+
+pub fn gcd_mul_div_round_up(x: &U, y: &U, denom: U) -> Option<U> {
+    let (x, y) = gcd_mul_div(x, y, denom)?;
+    if x.is_max() && y {
+        return None;
+    }
+    Some(if y { x + U::ONE } else { x })
+}
+
 pub fn widening_mul(x: &U, y: &U) -> [u8; 64] {
     let shift_128 = &U([
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -322,7 +353,7 @@ pub fn widening_mul(x: &U, y: &U) -> [u8; 64] {
     o
 }
 
-/// Widening mul div that's cheaper in codesize that's safe for cold
+/// Widening then truncate mul div that's cheaper in codesize that's safe for cold
 /// operations. The most expensive in gas costs.
 pub fn widening_mul_div(x: &U, y: &U, denom: U) -> Option<(U, bool)> {
     if denom.is_zero() {
@@ -943,12 +974,22 @@ impl U {
         widening_mul_div_round_up(self, y, z)
     }
 
+    #[cfg(feature = "ruint-enabled")]
     pub fn ruint_mul_div(&self, y: &Self, z: Self) -> Option<(Self, bool)> {
         ruint_mul_div(self, y, z)
     }
 
+    #[cfg(feature = "ruint-enabled")]
     pub fn ruint_mul_div_round_up(&self, y: &Self, z: Self) -> Option<Self> {
         ruint_mul_div_round_up(self, y, z)
+    }
+
+    pub fn gcd_mul_div(&self, y: &Self, z: Self) -> Option<(Self, bool)> {
+        gcd_mul_div(self, y, z)
+    }
+
+    pub fn gcd_mul_div_round_up(&self, y: &Self, z: Self) -> Option<Self> {
+        gcd_mul_div_round_up(self, y, z)
     }
 
     pub fn mul_mod(&self, y: &Self, z: &Self) -> Self {
