2da020a555e9eb490ca103f549d54da8ef9dad35
bayge <alex@fluidity.money>
2025-11-02 04:30:45 +0900

    Remove until the game starts

--- examples/004-bozo/src/pages/Game.tsx
+++ examples/004-bozo/src/pages/Game.tsx
@@ -763,7 +763,6 @@ export function Game() {
             }`}>
               {timeRemaining.formatted}
             </div>
-            <div className="text-sm text-muted-foreground mt-1">UNTIL THE GAME STARTS</div>
           </div>
         </div>
 
