2ff3734a4d6af6f8971ddb1000c77a584252a27d
bayge <alex@fluidity.money>
2025-11-17 07:52:32 -0300

    Allow unused messages in the panic code

--- bobcat-panic/src/lib.rs
+++ bobcat-panic/src/lib.rs
@@ -108,10 +108,9 @@ macro_rules! panic_on_err_bad_decoding_bool {
     }};
 }
 
-#[cfg(feature = "panic-revert")]
+#[allow(unused)]
 struct SliceWriter<'a>(&'a mut [u8], usize);
 
-#[cfg(feature = "panic-revert")]
 impl<'a> Write for SliceWriter<'a> {
     fn write_str(&mut self, s: &str) -> FmtResult {
         if self.1 + s.len() > self.0.len() {
@@ -126,7 +125,7 @@ impl<'a> Write for SliceWriter<'a> {
 /// Revert buffer size that's used to write the panic. We can afford to
 /// use a large page here since a panic will consume all the gas anyway,
 /// and a user will see this during simulation hopefully.
-#[cfg(feature = "panic-revert")]
+#[allow(unused)]
 const REVERT_BUF_SIZE: usize = 1024 * 10;
 
 #[cfg(all(feature = "panic", target_arch = "wasm32"))]
