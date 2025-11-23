8187ac09a1fbe49f8424fc159e0e60bf7db468ab
bayge <alex@fluidity.money>
2025-11-23 18:31:34 -0300

    Allow all build targets until we have a local implementation of ecrecover

--- bobcat-precompiles/src/lib.rs
+++ bobcat-precompiles/src/lib.rs
@@ -2,15 +2,12 @@
 
 use bobcat_maths::U;
 
-#[cfg(all(target_family = "wasm", target_os = "unknown"))]
 use bobcat_call::static_call_slice;
 
-#[allow(unused)]
 use array_concat::concat_arrays;
 
 type Address = [u8; 20];
 
-#[allow(unused)]
 pub const ADDR_ECRECOVER: Address = U::ONE.const_addr();
 
 // Upper bound of S to prevent malleability.
@@ -18,7 +15,6 @@ pub const ADDR_ECRECOVER: Address = U::ONE.const_addr();
 const ECRECOVER_MAX_S: U =
     U::const_from_hex(b"7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0").unwrap();
 
-#[cfg(all(target_family = "wasm", target_os = "unknown"))]
 pub fn ecrecover(hash: U, v: u8, r: U, s: U, gas: u64) -> Option<Address> {
     if v != 27 && v != 28 {
         return None;
