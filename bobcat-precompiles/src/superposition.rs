44228a5778bcc23bf1ef913ae90dc0d7fa4feb09
bayge <alex@fluidity.money>
2025-12-09 14:36:47 +0900

    Slightly increase gas ask for the ed25519 verify limit

--- bobcat-precompiles/src/superposition.rs
+++ bobcat-precompiles/src/superposition.rs
@@ -9,7 +9,7 @@ use array_concat::concat_arrays;
 /// EdVerify is deployed at this address to Arbitrum One and Superposition.
 pub const ADDR_EDVERIFY: [u8; 20] = address!(b"c3e443be2cfa4f41a5f5e4978d012847d355b419");
 
-const GAS_EDVERIFY: u64 = 92650;
+const GAS_EDVERIFY: u64 = 92715;
 
 pub fn edverify(digest: [u8; 64], pub_key: U, sig: [u8; 64]) -> bool {
     let cd: [u8; 64 * 2 + 32] = concat_arrays!(digest, pub_key.0, sig);
