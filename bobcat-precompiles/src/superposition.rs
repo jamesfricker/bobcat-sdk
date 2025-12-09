f80f9584630c821fc0cd72af11170562926f9ccf
bayge <alex@fluidity.money>
2025-12-09 19:30:22 +0900

    Slightly raise the default gas profile for edverify

--- bobcat-precompiles/src/superposition.rs
+++ bobcat-precompiles/src/superposition.rs
@@ -9,7 +9,9 @@ use array_concat::concat_arrays;
 /// EdVerify is deployed at this address to Arbitrum One and Superposition.
 pub const ADDR_EDVERIFY: [u8; 20] = address!(b"c3e443be2cfa4f41a5f5e4978d012847d355b419");
 
-const GAS_EDVERIFY: u64 = 92715;
+// Gas for the edverify function assumes the contract is a part of the
+// Stylus cache. If it's not, this may fail.
+const GAS_EDVERIFY: u64 = 93715;
 
 pub fn edverify(digest: [u8; 64], pub_key: U, sig: [u8; 64]) -> bool {
     let cd: [u8; 64 * 2 + 32] = concat_arrays!(digest, pub_key.0, sig);
