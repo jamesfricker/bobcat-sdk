9e23be38fc9b47b480bb7de1818a83bcca66803e
bayge <alex@fluidity.money>
2025-12-15 13:32:49 +0900

    Start to differentiate pre and post execution of ecrecover

--- bobcat-precompiles/src/ethereum.rs
+++ bobcat-precompiles/src/ethereum.rs
@@ -12,12 +12,20 @@ pub const ADDR_ECRECOVER: Address = U::ONE.const_addr();
 
 pub const ADDR_SECP256R1: Address = U::from_u32(256).const_addr();
 
+pub const GAS_ECRECOVER: u64 = u64::MAX;
+
+pub const GAS_SECP256: u64 = u64::MAX;
+
 // Upper bound of S to prevent malleability.
 #[allow(unused)]
 const ECRECOVER_MAX_S: U =
     U::const_from_hex(b"7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0").unwrap();
 
-pub fn ecrecover(hash: U, v: u8, r: U, s: U, gas: u64) -> Option<Address> {
+pub fn ecrecover_pre(preimage: &[u8], v: u8, r: U, s: U) -> Option<Address> {
+    ecrecover_post(keccak256(preimage), v, r, s)
+}
+
+pub fn ecrecover_post(hash: U, v: u8, r: U, s: U) -> Option<Address> {
     if v != 27 && v != 28 {
         return None;
     }
@@ -25,7 +33,7 @@ pub fn ecrecover(hash: U, v: u8, r: U, s: U, gas: u64) -> Option<Address> {
         return None;
     }
     let cd: [u8; 32 * 4] = concat_arrays!(hash.0, U::from(v).0, r.0, s.0);
-    let (rc, _, rd) = static_call_slice::<20>(ADDR_ECRECOVER, &cd, gas, 32 - 20);
+    let (rc, _, rd) = static_call_slice::<20>(ADDR_ECRECOVER, &cd, GAS_ECRECOVER, 32 - 20);
     if !rc {
         return None;
     }
@@ -34,7 +42,7 @@ pub fn ecrecover(hash: U, v: u8, r: U, s: U, gas: u64) -> Option<Address> {
 
 pub fn secp256r1_post(hash: U, r: U, s: U, qx: U, qy: U) -> Option<U> {
     let cd: [u8; 32 * 5] = concat_arrays!(hash.0, r.0, s.0, qx.0, qy.0);
-    let (rc, rd) = static_call_word(ADDR_SECP256R1, &cd, u64::MAX, 0);
+    let (rc, rd) = static_call_word(ADDR_SECP256R1, &cd, GAS_SECP256, 0);
     if !rc {
         return None;
     }
