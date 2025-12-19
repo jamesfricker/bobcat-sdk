bfbbc6cde20574141072175b079900da3b91b6c9
bayge <alex@fluidity.money>
2025-12-19 18:14:16 +0900

    Begin to add shadow event logging

--- bobcat-events/src/lib.rs
+++ bobcat-events/src/lib.rs
@@ -2,6 +2,7 @@
 
 pub use bobcat_maths::U;
 
+#[cfg(not(feature = "shadow"))]
 use array_concat::concat_arrays;
 
 #[cfg(all(target_family = "wasm", target_os = "unknown"))]
@@ -9,14 +10,58 @@ mod impls {
     #[link(wasm_import_module = "vm_hooks")]
     unsafe extern "C" {
         pub(crate) fn emit_log(data: *const u8, len: usize, topics: usize);
+        pub(crate) fn write_result(ptr: *const u8, len: usize);
     }
 }
 
 #[cfg(not(all(target_family = "wasm", target_os = "unknown")))]
+#[allow(unused)]
 mod impls {
     pub(crate) unsafe fn emit_log(_: *const u8, _: usize, _: usize) {}
+    pub(crate) unsafe fn write_result(_: *const u8, _: usize) {}
 }
 
+// ShadowTable that's intended for the 32 wasm machine to do shadow event
+// logging with custom Nitro instances with a whitelisted pool of nodes
+// to log from.
+#[derive(Debug, Clone, PartialEq)]
+#[repr(C)]
+struct ShadowTable<const DATA: usize> {
+    magic: u8,
+    topics_len: usize,
+    data_len: usize,
+    topics: [U; 5],
+    data: [u8; DATA],
+}
+
+const SHADOW_MAGIC_BYTE: u8 = 0xee;
+
+impl<const DATA: usize> Default for ShadowTable<DATA> {
+    fn default() -> Self {
+        ShadowTable {
+            magic: SHADOW_MAGIC_BYTE,
+            topics_len: 0,
+            data_len: 0,
+            topics: [U::ZERO; 5],
+            data: [0u8; DATA],
+        }
+    }
+}
+
+#[cfg(feature = "shadow")]
+pub fn emit_log_0_slice<const D: usize, const ALL: usize>(t0: &U, d: [u8; D]) {
+    let mut t = ShadowTable {
+        data_len: D,
+        data: d,
+        ..ShadowTable::<D>::default()
+    };
+    t.topics[0] = *t0;
+    unsafe {
+        impls::write_result(&t as *const _ as *const u8, 0);
+    }
+}
+
+#[cfg(not(feature = "shadow"))]
 pub fn emit_log_0_slice<const D: usize, const ALL: usize>(t0: &U, d: [u8; D]) {
     assert_eq!(ALL, D + 32, "not properly sized: {}", D + 32);
     let x: [u8; ALL] = concat_arrays!(t0.0, d);
@@ -25,6 +70,7 @@ pub fn emit_log_0_slice<const D: usize, const ALL: usize>(t0: &U, d: [u8; D]) {
     }
 }
 
+#[cfg(not(feature = "shadow"))]
 pub fn emit_log_1_slice<const D: usize, const ALL: usize>(t0: &U, t1: &U, d: [u8; D]) {
     assert_eq!(ALL, D + 32 * 2, "not properly sized: {}", D + 64);
     let x: [u8; ALL] = concat_arrays!(t0.0, t1.0, d);
@@ -33,6 +79,22 @@ pub fn emit_log_1_slice<const D: usize, const ALL: usize>(t0: &U, t1: &U, d: [u8
     }
 }
 
+#[cfg(feature = "shadow")]
+pub fn emit_log_1_slice<const D: usize, const ALL: usize>(t0: &U, t1: &U, d: [u8; D]) {
+    let mut t = ShadowTable {
+        topics_len: 1,
+        data_len: D,
+        data: d,
+        ..ShadowTable::<D>::default()
+    };
+    t.topics[0] = *t0;
+    t.topics[1] = *t1;
+    unsafe {
+        impls::write_result(&t as *const _ as *const u8, 0);
+    }
+}
+
+#[cfg(not(feature = "shadow"))]
 pub fn emit_log_2_slice<const D: usize, const ALL: usize>(t0: &U, t1: &U, t2: &U, d: [u8; D]) {
     assert_eq!(ALL, D + 32 * 3, "not properly sized: {}", D + 96);
     let x: [u8; ALL] = concat_arrays!(t0.0, t1.0, t2.0, d);
@@ -41,6 +103,23 @@ pub fn emit_log_2_slice<const D: usize, const ALL: usize>(t0: &U, t1: &U, t2: &U
     }
 }
 
+#[cfg(feature = "shadow")]
+pub fn emit_log_2_slice<const D: usize, const ALL: usize>(t0: &U, t1: &U, t2: &U, d: [u8; D]) {
+    let mut t = ShadowTable {
+        topics_len: 2,
+        data_len: D,
+        data: d,
+        ..ShadowTable::<D>::default()
+    };
+    t.topics[0] = *t0;
+    t.topics[1] = *t1;
+    t.topics[2] = *t2;
+    unsafe {
+        impls::write_result(&t as *const _ as *const u8, 0);
+    }
+}
+
+#[cfg(not(feature = "shadow"))]
 pub fn emit_log_3_slice<const D: usize, const ALL: usize>(
     t0: &U,
     t1: &U,
@@ -55,6 +134,30 @@ pub fn emit_log_3_slice<const D: usize, const ALL: usize>(
     }
 }
 
+#[cfg(feature = "shadow")]
+pub fn emit_log_3_slice<const D: usize, const ALL: usize>(
+    t0: &U,
+    t1: &U,
+    t2: &U,
+    t3: &U,
+    d: [u8; D],
+) {
+    let mut t = ShadowTable {
+        topics_len: 3,
+        data_len: D,
+        data: d,
+        ..ShadowTable::<D>::default()
+    };
+    t.topics[0] = *t0;
+    t.topics[1] = *t1;
+    t.topics[2] = *t2;
+    t.topics[3] = *t3;
+    unsafe {
+        impls::write_result(&t as *const _ as *const u8, 0);
+    }
+}
+
+#[cfg(not(feature = "shadow"))]
 pub fn emit_log_4_slice<const D: usize, const ALL: usize>(
     t0: &U,
     t1: &U,
@@ -70,7 +173,32 @@ pub fn emit_log_4_slice<const D: usize, const ALL: usize>(
     }
 }
 
-#[cfg(feature = "alloc")]
+#[cfg(feature = "shadow")]
+pub fn emit_log_4_slice<const D: usize, const ALL: usize>(
+    t0: &U,
+    t1: &U,
+    t2: &U,
+    t3: &U,
+    t4: &U,
+    d: [u8; D],
+) {
+    let mut t = ShadowTable {
+        topics_len: 4,
+        data_len: D,
+        data: d,
+        ..ShadowTable::<D>::default()
+    };
+    t.topics[0] = *t0;
+    t.topics[1] = *t1;
+    t.topics[2] = *t2;
+    t.topics[3] = *t3;
+    t.topics[4] = *t4;
+    unsafe {
+        impls::write_result(&t as *const _ as *const u8, 0);
+    }
+}
+
+#[cfg(all(feature = "alloc", not(feature = "shadow")))]
 pub fn emit_log_0_vec(t0: &U, d: &[u8]) {
     let mut x = t0.to_vec();
     x.extend_from_slice(d);
@@ -79,7 +207,7 @@ pub fn emit_log_0_vec(t0: &U, d: &[u8]) {
     }
 }
 
-#[cfg(feature = "alloc")]
+#[cfg(all(feature = "alloc", not(feature = "shadow")))]
 pub fn emit_log_1_vec(t0: &U, t1: &U, d: &[u8]) {
     let mut x = t0.to_vec();
     x.extend_from_slice(&t1.0);
@@ -89,7 +217,7 @@ pub fn emit_log_1_vec(t0: &U, t1: &U, d: &[u8]) {
     }
 }
 
-#[cfg(feature = "alloc")]
+#[cfg(all(feature = "alloc", not(feature = "shadow")))]
 pub fn emit_log_2_vec(t0: &U, t1: &U, t2: &U, d: &[u8]) {
     let mut x = t0.to_vec();
     x.extend_from_slice(&t1.0);
@@ -100,7 +228,7 @@ pub fn emit_log_2_vec(t0: &U, t1: &U, t2: &U, d: &[u8]) {
     }
 }
 
-#[cfg(feature = "alloc")]
+#[cfg(all(feature = "alloc", not(feature = "shadow")))]
 pub fn emit_log_3_vec(t0: &U, t1: &U, t2: &U, t3: &U, d: &[u8]) {
     let mut x = t0.to_vec();
     x.extend_from_slice(&t1.0);
@@ -112,7 +240,7 @@ pub fn emit_log_3_vec(t0: &U, t1: &U, t2: &U, t3: &U, d: &[u8]) {
     }
 }
 
-#[cfg(feature = "alloc")]
+#[cfg(all(feature = "alloc", not(feature = "shadow")))]
 pub fn emit_log_4_vec(t0: &U, t1: &U, t2: &U, t3: &U, t4: &U, d: &[u8]) {
     let mut x = t0.to_vec();
     x.extend_from_slice(&t1.0);
